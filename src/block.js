254481cab31544da1149d5849b01f6d0f5eefa7a
Adam Gee <adamgee@Adams-MacBook-Pro.local>
2020-04-24 12:43:39 +0800

    Add block class to create a city like landscape

---
+++ src/block.js
@@ -0,0 +1,26 @@
+import * as THREE from "three";
+
+class Block {
+
+    constructor(options) {
+        this.options = options;
+        return this.createMesh();
+    }
+
+    createMesh() {
+        let height = 4;
+        const geometry = new THREE.BoxBufferGeometry( this.options.width, this.options.height, this.options.depth );
+        
+        // create a Mesh containing the geometry and material
+        let mesh = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial({color:0xffffff}) );
+        mesh.position.set(this.options.x, this.options.y, this.options.z );
+        mesh.receiveShadow = true;
+        mesh.castShadow = true;
+        
+        return mesh;
+    }
+
+
+}
+
+export default Block;
--- src/digital.popup.js
+++ src/digital.popup.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 var OrbitControls = require('three-orbit-controls')(THREE);
+import Block from "./block";
 
 
 class DigitalPopup {
@@ -28,7 +29,7 @@ class DigitalPopup {
         const fov = 35; // fov = Field Of View
         const aspect = container.clientWidth / container.clientHeight;
         const near = 0.1;
-        const far = 100;
+        const far = 300;
 
         this.camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
 
@@ -37,43 +38,59 @@ class DigitalPopup {
         this.camera.position.set( 0, 0, 10 );
 
         // create a geometry
-        const geometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
-        
-        // create a Mesh containing the geometry and material
-        var mesh = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME}) );
+        const geometry = new THREE.BoxBufferGeometry( 2, 4, 2 );
+    
 
-        mesh.position.y = 1;
-        // add the mesh to the scene object
-        mesh.receiveShadow = true;
-	    mesh.castShadow = true;
-        this.scene.add( mesh );
+        this.addRandomBlocks();
 
         // create a Mesh containing the geometry and material
-        var meshFloor = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100, 20, 20 ), new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME}) );
-
-        // add the mesh to the scene object
-        this.scene.add( mesh );
-
+        var meshFloor = new THREE.Mesh( new THREE.PlaneGeometry( 100, 100, 100, 100 ), new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME}) );
 
         meshFloor.rotation.x -= Math.PI / 2;
 	   // Floor can have shadows cast onto it
 	    meshFloor.receiveShadow = true;
         // add the mesh to the scene object
         this.scene.add( meshFloor );
-        
+    
+        var meshWall1 = new THREE.Mesh( new THREE.PlaneGeometry( 100, 40, 20, 20 ), new THREE.MeshPhongMaterial({color:0xffffff, side: THREE.DoubleSide, wireframe:USE_WIREFRAME}) );
+        meshWall1.position.z = -50;
+        meshWall1.position.y = 20;
+	    meshWall1.receiveShadow = true;
+        this.scene.add( meshWall1 );
 
-        const ambientLight = new THREE.AmbientLight( 0xffffff, 0.90);
+        var meshWall2 = new THREE.Mesh( new THREE.PlaneGeometry( 100, 40, 20, 20 ), new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME, side: THREE.DoubleSide}) );
+        meshWall2.position.z = 50;
+        meshWall2.position.y = 20;
+	    meshWall2.receiveShadow = true;
+        this.scene.add( meshWall2 );
+
+        var meshWall3 = new THREE.Mesh( new THREE.PlaneGeometry( 100, 40, 20, 20 ), new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME, side: THREE.DoubleSide}) );
+        meshWall3.rotation.y -= Math.PI / 2;
+        meshWall3.position.x = -50;
+        meshWall3.position.y = 20;
+	    meshWall3.receiveShadow = true;
+        this.scene.add( meshWall3 );
+
+        var meshWall4 = new THREE.Mesh( new THREE.PlaneGeometry( 100, 40, 20, 20 ), new THREE.MeshPhongMaterial({color:0xffffff, wireframe:USE_WIREFRAME, side: THREE.DoubleSide}) );
+        meshWall4.rotation.y -= Math.PI / 2;
+        meshWall4.position.x = 50;
+        meshWall4.position.y = 20;
+	    meshWall4.receiveShadow = true;
+        this.scene.add( meshWall4 );
+
+        const ambientLight = new THREE.AmbientLight( 0xffffff, 0.85);
         // remember to add the light to the scene
         this.scene.add( ambientLight );
 
         // Create a directional light
-        const light = new THREE.PointLight( 0xffffff, 0.1, 40 );
+        const light = new THREE.PointLight( 0xffffff, 0.15, 120 );
         light.castShadow = true;
         light.shadow.camera.near = 0.1;
-        light.shadow.camera.far = 105;
-
+        light.shadow.camera.far = 300;
+        light.shadow.mapSize.width = 1024;
+        light.shadow.mapSize.height = 1024;
         // move the light back and up a bit
-        light.position.set( -2, 10, -10 );
+        light.position.set( 0, 60, -10 );
 
         // remember to add the light to the scene
         this.scene.add( light );
@@ -84,7 +101,7 @@ class DigitalPopup {
 
         // Enable Shadows in the Renderer
         this.renderer.shadowMap.enabled = true;
-        this.renderer.shadowMap.type = THREE.BasicShadowMap;
+        this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
         this.renderer.setPixelRatio( window.devicePixelRatio );
 
@@ -92,6 +109,23 @@ class DigitalPopup {
         container.appendChild( this.renderer.domElement );
 
         this.controls = new OrbitControls( this.camera, this.renderer.domElement );
+    }
+
+    addRandomBlocks() {
+        for(var i = 0; i < 100; i++) {
+            let height = this.random( 1, 11);
+            let base = this.random( 2, 3);
+            var mesh = new Block({
+                width: base,
+                height: height,
+                depth: base,
+                x: this.random( -40, 40),
+                y: height/2,
+                z: this.random( -40, 40)
+            });
+            this.scene.add( mesh );
+        }
+        
     }
 
     animate() {
@@ -105,6 +139,10 @@ class DigitalPopup {
         // function calls itself
         this.renderer.render( this.scene, this.camera );
       
+    }
+
+    random(min, max) {
+        return Math.floor(Math.random() * (max - min) + min);
     }
 
 }
