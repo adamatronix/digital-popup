15c44cdacff340c2596c9889ce905a52c888087c
adamatronix <you@example.com>
2022-04-23 23:40:30 +0800

    master Add new stuff for prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,22 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const TerserPlugin = require('terser-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
    mode: 'production',
    devtool: 'source-map',
+   plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Digital Popup',
+      template: './src/index.html'
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
    optimization: {
     minimize: true,
     minimizer: [new TerserPlugin()],
